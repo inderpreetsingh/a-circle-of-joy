9c3857422583c5faf33778675baa5f983fd217e7
Inderpreet Singh <singhs.ishwer@gmail.com>
2015-12-02 19:18:54 +0530

    solved the missing brackets error

--- acoj/client/auth/forgotpassword.js
+++ acoj/client/auth/forgotpassword.js
@@ -2,7 +2,7 @@ Template.forgotPassword.events({
   'submit form': function(e,t){
     event.preventDefault();
     var email = $('[name=email]').val();
-    if isNotEmpty (email) {
+    if (isNotEmpty (email)) {
       Accounts.forgotPassword({email:email},function(err){
         if (err) {
           sAlert.error(err.reason);
